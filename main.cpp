e3787ff7c14d39c2ba3bb818b547ffc770a17cb5
Trinh To <91444313+TrinhBTo@users.noreply.github.com>
2021-10-01 12:41:35 -0700

    [VSC]

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* Trinh To 
+    Visual Studio Code Assignment
+*/
+#include <iostream>
+
+using namespace std;
+
+void master();
+
+int main() {
+    cout << "Hello World" << endl;
+    master();
+    return 0;
+}
---
+++ utilities.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+
+using namespace std;
+
+// defined in utilities.cpp
+void master() { cout << "Beginners very first program." << endl; }
